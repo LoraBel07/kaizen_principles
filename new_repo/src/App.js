1cd269e801ac4d240e7bc875953d296b27d08a53
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-01-29 18:34:24 -0500

    commit

--- new_repo/src/App.js
+++ new_repo/src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
+import { data } from "./data";  
 import './App.css';
 
 function App() {
+  const [person, setPerson] = useState(0);
+  const { id, name, description, age, image } = data[person];
+  console.log(data[person]);
+
   return (
     <div className="App">
-      
+      <h1>TEST</h1>
     </div>
   );
 }
