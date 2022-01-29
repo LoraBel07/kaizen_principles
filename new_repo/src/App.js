a02d92f0c354ecf229d1fd472cbe8f30367d1fb2
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-01-29 18:47:05 -0500

    commit

--- new_repo/src/App.js
+++ new_repo/src/App.js
@@ -5,13 +5,26 @@ import './App.css';
 function App() {
   const [person, setPerson] = useState(0);
   const { id, name, description, age, image } = data[person];
-  console.log(data[person]);
+  // console.log(data[person]);
 
-  return (
-    <div className="App">
-      <h1>TEST</h1>
-    </div>
-  );
+  return (<div>
+      <div className="container">
+        <img src={image} width="500px" alt="person"/>
+      </div>
+
+      <div className="container">
+        <h1>{id} {name}</h1>
+      </div>
+
+      <div className="container">
+        <h2>{description}</h2>
+      </div>
+
+      <div className="container">
+        <h2>{age}</h2>
+      </div>
+
+    </div>);
 }
 
 export default App;
