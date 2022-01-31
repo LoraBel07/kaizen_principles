ac74d7ff17df05c0d88fc4089ffa5f65f6155de6
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-01-30 23:04:41 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import { data, dataTwo } from "./data";  
+import './App.css';
+
+function App() {
+  const [principles, setPrinciple] = useState(0);
+  const { id, name, description, image } = data[principles];
+
+  const [types, setTypes] = useState(dataTwo);
+  
+  const setShowMore = (num) => {
+    const newTypes = [];
+    types.forEach(types => {
+      if (types.num === num) {
+        const changedTypes = { ... types, showMore: !types.showMore};
+        newTypes.push(changedTypes);
+      } else {
+        newTypes.push(types);
+      }
+    });
+    setTypes(newTypes);
+  }
+
+  const removeType = (num) => {
+    let newTypes = types.filter(types => types.num !== num);
+    setTypes(newTypes);
+  }
+
+
+  const previousPrinciple = () => {
+    setPrinciple((principle => {
+      principle--;
+      if (principle < 0) {
+        return data.length - 1;
+      }
+      return principle;
+    }))
+  }
+
+  const nextPrinciple = () => {
+    setPrinciple((principle =>{
+      principle++;
+      if (principle > data.length - 1) {
+        principle = 0;
+      }
+      return principle;
+    }))
+  }
+  
+  return (<div>
+      <div className="container">
+        <h1> カイゼン </h1>
+      </div>
+      
+      <div className="container">
+        <h2>10 Basic Principles of  K A I Z E N</h2>
+      </div>
+
+      <div className="container">
+        <img src={image} height="200px" alt="principle"/>
+      </div>
+
+      <div className="container">
+        <h2>{id}. {name}</h2>
+      </div>
+
+      <div className="btn container">
+        <button className="pointers" onClick={previousPrinciple}><img src="https://img.icons8.com/ios-filled/30/ffffff/hand-left.png" alt="previous" /></button>
+        <button className="pointers" onClick={nextPrinciple}><img src="https://img.icons8.com/ios-filled/30/ffffff/hand-right.png" alt="next" /></button>
+      </div>
+
+      <div className="container description">
+      <p>{ description }</p>
+      </div>  
+
+      <div className='container'>
+          <h2>The { types.length } Types of Kaizen.</h2>
+      </div> 
+      <div className='par container'>
+      {types.map((element => {
+        const { num, header, meaning, showMore } = element;
+        return(
+          <div className='parBox' key={num}>
+            <div className="header">
+              <h3>{num}. {header}</h3>
+            </div>
+            <div className="container">
+            <p>{ showMore ? meaning.substring(0,90) + "..." : meaning }                              
+              <button className="show" onClick={() => setShowMore(num)}>{showMore ? "Show more" : "Show less"}</button>
+            </p>
+            </div>
+            <div className="container">
+              <button className="btn" onClick={() => removeType(num)}>Remove</button>
+            </div>
+
+          </div>
+        )
+      }))} 
+      </div>
+      <div className="container">
+        <button className="btn" onClick={() => setTypes([])}>Delete all</button>
+      </div>
+
+
+
+      <div className="container">
+        <h4> ⭐ "Try to do something just a little bit better each day in order to make a large impact in the long run." ⭐</h4>
+      </div>
+    </div>);
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,104 @@
+import image1 from "./moving.jpg";
+import image2 from "./proactive.jpg";
+import image3 from "./bye.jpg";
+import image4 from "./improve.jpg";
+import image5 from "./correction.jpg";
+import image6 from "./speak.png";
+import image7 from "./method.jpg";
+import image8 from "./why.jpg";
+import image9 from "./money.png";
+import image10 from "./crowdsource.jpg";
+
+export const data = [
+	{
+		id: 1,
+		name: "Never Stop",
+		description: "There is an area for improvement in everyone’s life, no matter how successful they are. The idea here is to focus on continuous improvements in your life, every day, no matter how small the step you take to be a better you than you were yesterday.",
+		image: image1
+	},
+
+	{
+		id: 2,
+		name: "Be Proactive",
+		description: "Stop hesitating because of possible hurdles that you predict, and move forward toward your goal for continuous improvement. It is important to not take any chance in the way of improvement, and for each person in the organization to take personal responsibility for making improvements. While it is one thing to sit around and discuss about the improvements that should be made, it is different thing to actually make an action plan to implement the changes.",
+		image: image2
+	},
+
+	{
+		id: 3,
+		name: "Eliminate Old Practices",
+		description: "The main part of avoiding complacency is getting rid of old and tired traditions that are obsolete. Because we live in a culture that continually making changes and improvements, it is important for businesses to keep up with this in order to stay relevant  for company’s continuous improvement . To get rid of old practices throughout  your life,  find out about step up your day  by improving upon things that you already do. Let go of your former habits, even if they give you some benefit at the time. As time goes, you should progress also.",
+		image: image3	
+	},
+
+	{
+		id: 4,
+		name: "Don’t Stop",
+		description: "Never consider yourself to be finished. Improvement has no limits, and can be proceeded on an infinite level. You can always improve yourself. Whether it is learning something new useful things or perfecting a skill you already have, there is opportunity to get better in everybody’s life for continuous improvement.",
+		image: image4
+	},
+
+	{
+		id: 5,
+		name: "Make Corrections",
+		description: "Some Part of the trial-and-error process requires making corrections to practices or processes that have a major issue with them. It is critical to continue to make corrections all through the process in order to continuous improvement. To Start some new habits to correct the things that you may have been doing wrong up until this point. Observe the areas in your life that can use improvement, and make relevant changes to help you progress.",
+		image: image5	
+	},
+	{
+		id: 6,
+		name: "Empower All Employees to Speak Up",
+		description: "Everyone who is associated in the success of the company—from the CEO to the line workers—should be encouraged to offer suggestions and solutions for problems that can be fixed, or improvements that can be made. While you don’t have employees dealing with your own life, you can still energize your friends and family to make suggestions if they see things that you could work on. Talk with different people about your goals and get their feedback. Encourage your family and friends to make suggestions if they see things that you could work on.",
+		image: image6
+	},
+	{
+		id: 7,
+		name: "Don’t Assume New Methods Will Work",
+		description: "Just because a method is new does not mean that it is improved. It is main things to move far beyond the status quo in order to be effective in business. This will probably require trial and error until achievement is accomplished. When it comes to your personal life, it might take some time to find a method that works for you when it comes to achieving your goals. Just Because something worked for another person does not imply that it will essentially work for you. Try a few things out before you focus to a new method.",
+		image: image7
+	},
+	{
+		id: 8,
+		name: "Practice the “Five Why” Method",
+		description: "Before making any final decisions, get to the root of the problem by asking “why?” five times. By continuing to ask yourself “why?” you are likely to find the genuine purpose behind a problem. Often, the assumed reason for a problem will actually lead to another question and dig deeper into the issue. possibly you have been telling yourself that you don’t have time to work out, and that is the reason you are out of shape.",
+		image: image8
+	},
+	{
+		id: 9,
+		name: "Be Economical",
+		description: "Watch your finances during the entire process of making an improvement. Look for ways to save money while making minor improvements, and then spend the saved money on further improvements. A common goal of improvement is to save money or stop spending so much money on unuseful Things. And stop buying them.",
+		image: image9
+	},
+	{
+		id: 10,
+		name: "Crowdsource",
+		description: "Learn from other people and get some new opinions on things you can add to your routine to create continuous improvement. Before making a change or improvement, Talk with different people about what they are doing in their daily life to improve themselves. You will learn from different people and get some new opinions on things you can add to your routine to create continuous improvement. Having multiple opinions can not only add to an improvement, but it may also offer a different perspective that will open up more new ideas.",
+		image: image10
+	}
+	]
+
+	export const dataTwo = [
+		{
+			num: 1,
+			header: "KAIZEN TEIAN",
+			meaning: "Kaizen Teian: Bottom-Up Improvement. Kaizen Teian describes a form of improvement where people participate to improve their own processes. This bottom-up type of Kaizen drives a cultural transformation because it requires everyone to think about improvement every day, everywhere. At its core, Kaizen Teian is about actively involving all people in improvement. If you want to set out in creating a continuous improvement culture in your organization, you should start with Kaizen Teian.",
+			showMore: "Kaizen Teian: Bottom-Up Improvement. Kaizen Teian describes a form of improvement where people participate to improve their own processes. This bottom-up type of Kaizen drives a cultural transformation because it requires everyone to think about improvement every day, everywhere. At its core, Kaizen Teian is about actively involving all people in improvement. If you want to set out in creating a continuous improvement culture in your organization, you should start with Kaizen Teian."
+		},
+		{
+			num: 2,
+			header: "KAIZEN EVENTS",
+			meaning: "Kaizen Events: Defined Improvements. Unlike daily Kaizen, a Kaizen event is not about continuous improvement—it’s about a specific process improvement developed over a brief amount of time. Kaizen events are typically brief, focused improvement projects where people, including the management team, participate in analyzing their Value Stream Map (VSM) to solve a specific problem. While daily Kaizen projects may be small in scope or somewhat spontaneous, a Kaizen event demands thoughtful planning by team leaders involved.",
+			showMore: "Kaizen Events: Defined Improvements. Unlike daily Kaizen, a Kaizen event is not about continuous improvement—it’s about a specific process improvement developed over a brief amount of time. Kaizen events are typically brief, focused improvement projects where people, including the management team, participate in analyzing their Value Stream Map (VSM) to solve a specific problem. While daily Kaizen projects may be small in scope or somewhat spontaneous, a Kaizen event demands thoughtful planning by team leaders involved. "
+		},
+		{
+			num: 3,
+			header: "KAIKAKU",
+			meaning: "Kaikaku: Radical Change. Sometimes small changes aren’t enough to drive the improvements an organization needs to be competitive. That’s when it’s time to turn to Kaikaku. Unlike Kaizen, which focuses on incremental changes, Kaikaku describes a process where an entire organization is focused on a radical process transformation. Rather than improving a process, Kaikaku may demand the organization moves to an entirely new process. Examples of Kaikaku might include moving from manual to automated production or embarking on a digital transformation that drives workplace collaboration.",
+			showMore: "Kaikaku: Radical Change. Sometimes small changes aren’t enough to drive the improvements an organization needs to be competitive. That’s when it’s time to turn to Kaikaku. Unlike Kaizen, which focuses on incremental changes, Kaikaku describes a process where an entire organization is focused on a radical process transformation. Rather than improving a process, Kaikaku may demand the organization moves to an entirely new process. Examples of Kaikaku might include moving from manual to automated production or embarking on a digital transformation that drives workplace collaboration."
+		},
+		{
+			num: 4,
+			header: "KAKUSHIN",
+			meaning: "Kakushin: Break-through Innovation. If Kaikaku is revolutionary, Kakushin is game-changing. Kakushin happens when you move to an entirely new way of doing something. It’s about the big break-through that changes everything. While Kaikaku might mean making a major change in how things are done, Kakushin might be about changing what is done. For example, if Kaikaku is about moving from manual to automated production, Kakushin would be the switch to 3D printing those materials, demanding new skills from the workforce.",
+			showMore: "Kakushin: Break-through Innovation. If Kaikaku is revolutionary, Kakushin is game-changing. Kakushin happens when you move to an entirely new way of doing something. It’s about the big break-through that changes everything. While Kaikaku might mean making a major change in how things are done, Kakushin might be about changing what is done. For example, if Kaikaku is about moving from manual to automated production, Kakushin would be the switch to 3D printing those materials, demanding new skills from the workforce."
+		}
+	]
